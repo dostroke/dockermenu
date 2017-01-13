efc9fbb5e9c58e80c0eb20de3f568c79fe32ecd4
humax <humax@dockerir.humaxdigital.com>
2017-01-13 16:02:07 +0900

    Add get toolchains

--- vminitialize/docker_envinit.sh
+++ vminitialize/docker_envinit.sh
@@ -2,13 +2,13 @@
 
 ### INSTALL PYTHON
 su humax
-sudo apt-get update ; sudo apt-get install python
+sudo apt-get update ; sudo apt-get install -y python
 
 ### INSTALL DOCKER ENGINE
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' -y
 sudo apt-get update
-sudo apt-get install docker-engine
+sudo apt-get install -y docker-engine
 sudo usermod -aG docker humax
 
 ### NOPASSWD SETTING
@@ -18,9 +18,9 @@ sudo sed -i '26d' /etc/sudoers
 ### CONFIGURE DOCKER DAEMON
 sudo mkdir -p /etc/systemd/system/docker.service.d
 sudo touch /etc/systemd/system/docker.service.d/docker.conf
-echo -e "[Service]" >> /etc/systemd/system/docker.service.d/docker.conf
-echo -e "ExecStart=" >> /etc/systemd/system/docker.service.d/docker.conf
-echo -e "ExecStart=/usr/bin/docker daemon -H fd:// --insecure-registry 10.0.218.196:5000" >> /etc/systemd/system/docker.service.d/docker.conf
+sudo bash -c 'echo -e "[Service]" >> /etc/systemd/system/docker.service.d/docker.conf'
+sudo bash -c 'echo -e "ExecStart=" >> /etc/systemd/system/docker.service.d/docker.conf'
+sudo bash -c 'echo -e "ExecStart=/usr/bin/docker daemon -H fd:// --insecure-registry 10.0.218.195:5000" >> /etc/systemd/system/docker.service.d/docker.conf'
 
 ### RESTART DAEMON
 sudo systemctl daemon-reload
@@ -41,11 +41,10 @@ sudo chmod 777 /nfsroot
 sudo chmod 777 /tftpboot
 
 ### COPY TOOLCHAINS
-sudo apt-get update ; sudo apt-get install -y sshpass
-sudo mkdir -p /opt/toolchains
-sudo chmod 777 /opt/toolchains
-cd /opt/toolchains
-sshpass -p "humax12@!" scp -r humax@10.0.218.196:/opt/toolchains/* .
+while readline
+do
+	wget http://10.0.14.206/files/cis/compilers/$line
+done < toolchains.txt
 
 ### INSTALL SAMBA
 sudo apt-get update
