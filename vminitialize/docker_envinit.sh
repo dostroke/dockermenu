d40a991c6dd571589a43deff756e175db957825f
Sangjin <sjkim5@humaxdigital.com>
2017-03-03 10:53:55 +0900

    change docker installation method

--- vminitialize/docker_envinit.sh
+++ vminitialize/docker_envinit.sh
@@ -11,16 +11,25 @@ NC='\033[0m'
 #su humax
 sudo apt-get update ; sudo apt-get install -y python
 
-### INSTALL DOCKER ENGINE
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' -y
+### INSTALL DOCKER ENGINE / Deprecated from 2017.3.3
+#sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+#sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main' -y
+#sudo apt-get update
+#sudo apt-get install -y docker-engine
+#sudo usermod -aG docker humax
+
+### INSTALL Docker Community Edition / 2017.3.3
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install -y docker-engine
+sudo apt-get install -y docker-ce
 sudo usermod -aG docker humax
 
 ### NOPASSWD SETTING
 sudo sed -i '26a\\%sudo\tALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
-sudo sed -i '26d' /etc/sudoers
+isudo sed -i '26d' /etc/sudoers
 
 ### CONFIGURE DOCKER DAEMON
 sudo mkdir -p /etc/systemd/system/docker.service.d
